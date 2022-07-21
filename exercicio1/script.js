cb407ec825ff3b265cda54972b2c916b903f9655
Bruno Carvalho <brunopcarvalho0606@gmail.com>
2022-07-21 18:13:15 -0300

    Feito Exercício 1

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const par = +prompt('Digite um número par:');
+
+console.log('Resto do nº div por 2:', par%2);
+
+// Com números pares sempre resta 0, pois par/par é exata.
+// Com números impares sempre resta 1, pois impar/par não é exata.
+
